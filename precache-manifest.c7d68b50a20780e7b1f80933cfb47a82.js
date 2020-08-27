8bfcbd70a2697f21995b2ba4aa15a961266bdad8
ganesh-su <ganeshkumar3199@gamil.com>
2020-08-27 22:44:08 +0530

    Updates

---
+++ precache-manifest.c7d68b50a20780e7b1f80933cfb47a82.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d40fe255ba74987fe9e8708e0dcd3e8d",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "db673a898b5c5b1980ec",
+    "url": "/my-react-app/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "c729293440370bd54865",
+    "url": "/my-react-app/static/css/main.97b6026e.chunk.css"
+  },
+  {
+    "revision": "db673a898b5c5b1980ec",
+    "url": "/my-react-app/static/js/2.4bf21290.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-react-app/static/js/2.4bf21290.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c729293440370bd54865",
+    "url": "/my-react-app/static/js/main.f8fb5718.chunk.js"
+  },
+  {
+    "revision": "435064591b6543e8ca17",
+    "url": "/my-react-app/static/js/runtime-main.c0584337.js"
+  },
+  {
+    "revision": "a091a231121d7a1692d919399f6937b2",
+    "url": "/my-react-app/static/media/ganesh.a091a231.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-react-app/precache-manifest.3a84095ed21d6a2d28055d547e9428b2.js"
+  "/my-react-app/precache-manifest.c7d68b50a20780e7b1f80933cfb47a82.js"
 );
 
 self.addEventListener('message', (event) => {
